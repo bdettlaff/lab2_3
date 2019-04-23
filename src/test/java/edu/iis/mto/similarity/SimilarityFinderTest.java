dbca44d7cc94ac36082cd76a1c46b1c0233064ba
dettlaffb <209295@edu.p.lodz.pl>
2019-04-23 23:12:34 +0200

    test_v3 First Test, should return true if counter has value six

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,61 +1,21 @@
 package edu.iis.mto.similarity;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class SimilarityFinderTest {
 
-    private SimilarityFinder similarityFinder;
-
-    @Before
-    public void setUp() {
-        this.similarityFinder = new SimilarityFinder(new SequenceSearcherDubler());
-    }
-
     @Test
-    public void shouldReturnTrueIfBothArraysLengthIsZero() {
-        int[] firstSequence = new int[0];
-        int[] secondSequence = new int[0];
-
-        assertEquals(1.0d, similarityFinder.calculateJackardSimilarity(firstSequence, secondSequence), 0.01);
-    }
-
-    @Test
-    public void shouldReturnTrueIfBothArraysHaveTheSameValues() {
+    public void shouldReturnTrueIfCounterHasValueSix() {
         int[] firstSequence = {1, 2, 3, 6, 8, 9};
         int[] secondSequence = {1, 2, 3, 6, 8, 9};
 
-        assertEquals(similarityFinder.calculateJackardSimilarity(firstSequence, secondSequence), 1.0d, 0.01);
-    }
+        SequenceSearcherDouble sequenceSearcherDouble = new SequenceSearcherDouble();
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherDouble);
 
-    @Test
-    public void shouldReturnTrueIfAmountOfInvokesIsSix() {
-        int[] firstSequence = {1, 2, 3, 6, 8, 9};
-        int[] secondSequence = {1, 2, 3, 6, 8, 9};
-
-        SequenceSearcherDubler sequenceSearcherDubler = new SequenceSearcherDubler();
-        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherDubler);
         similarityFinder.calculateJackardSimilarity(firstSequence,secondSequence);
-
-        assertEquals(sequenceSearcherDubler.getCallCounter(),6);
-    }
-
-    @Test
-    public void shouldReturnTrueIfThereIsNoSimilarityBetweenArrays(){
-        int[] firstSequence = {1, 2, 3, 6, 8, 9};
-        int[] secondSequence = {4, 5, 10, 11, 12, 13};
-
-        assertEquals(similarityFinder.calculateJackardSimilarity(firstSequence,secondSequence),0.0,0.01);
-    }
-
-    @Test
-    public void shouldReturnTrueIfSecondArrayIsEmpty(){
-        int[] firstSequence = {1, 2, 3, 6, 8, 9};
-        int[] secondSequence = {};
-
-        assertEquals(similarityFinder.calculateJackardSimilarity(firstSequence,secondSequence),0.0,0.01);
+        assertEquals(sequenceSearcherDouble.getCounter(), 6);
     }
 
 }
